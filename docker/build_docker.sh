12d18e5c4ef5130743a75b944561b3649046da59
RamiKrispin <ramkrisp@umich.edu>
2023-04-30 21:25:45 -0700

    add docker settings

---
+++ docker/build_docker.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+echo "Build the docker"
+
+docker build . --progress=plain 
+               -t rkrispin/linkedin_dash:0.0.0.9000
+
+if [[ $? = 0 ]] ; then
+echo "Pushing docker..."
+docker push rkrispin/linkedin_dash:0.0.0.9000
+else
+echo "Docker build failed"
+fi
