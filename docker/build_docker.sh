95739d5fafee5a4026eb27e4924153d9e30654b2
RamiKrispin <ramkrisp@umich.edu>
2023-04-30 22:07:19 -0700

    add docker-compose and update the docker image files

--- docker/build_docker.sh
+++ docker/build_docker.sh
@@ -2,8 +2,7 @@
 
 echo "Build the docker"
 
-docker build . --progress=plain 
-               -t rkrispin/linkedin_dash:0.0.0.9000
+docker build . --progress=plain -t rkrispin/linkedin_dash:0.0.0.9000
 
 if [[ $? = 0 ]] ; then
 echo "Pushing docker..."
